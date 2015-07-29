78c0138cd09bcdb01f0ea33462a0ea41dacdb7f6
parulsharda <sharmanancy007@gmail.com>
2015-07-29 12:57:13 +0530

    Application displays the List of books with Author name and published date

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,23 +2,35 @@ package com.twu.biblioteca;
 
 import java.util.ArrayList;
 
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
         IO ioScreen = new IO();
-        ioScreen.welcome();
-
-
-        System.out.println("\nThe Books in Library are:");
+        ArrayList<ArrayList<String>> collection = new ArrayList<ArrayList<String>>();
+        ArrayList<String> libraryBook1 = new ArrayList<String>();
+        ArrayList<String> libraryBook2 = new ArrayList<String>();
 
         ArrayList<String> booksList = new ArrayList<String>();
         booksList.add("The Hunger Games");
         booksList.add("Harry Potter");
         booksList.add("Gone With the Wind");
 
-        ioScreen.showBooks(booksList);
+        libraryBook1.add("Games of Thrones");
+        libraryBook1.add("George R. R. Martin");
+        libraryBook1.add("1996");
+        collection.add(libraryBook1);
 
+        libraryBook2.add("Gone With the Wind ");
+        libraryBook2.add("Margaret Mitchell");
+        libraryBook2.add("1936");
+        collection.add(libraryBook2);
+
+        ioScreen.welcome();
+        //ioScreen.showBooks(booksList);
+        ioScreen.showDetailsOfBook(collection);
     }
 }
+
 
--- src/com/twu/biblioteca/IO.java
+++ src/com/twu/biblioteca/IO.java
@@ -10,7 +10,22 @@ public class IO {
     }
 
     public void showBooks(ArrayList<String> booksList) {
+        System.out.println("\nThe Books in Library are:");
         for(String books : booksList)
-            System.out.println(" "+books);
+            System.out.println(" " + books);
     }
+
+    public void showDetailsOfBook(ArrayList<ArrayList<String>> collection) {
+        System.out.print("\n Books Details:\n|\t\t\tName\t\t\t|\t\t\tAuthor\t\t\t|\tYera\t\t|\n");
+        System.out.println(" - - - - - - - - - - - - - -- - - -- - - - -- - - - - - - - - - - - - - -");
+        for(int i = 0 ; i < collection.size() ; i++) {
+            System.out.print("|");
+            ArrayList<String> currentList = collection.get(i);
+            for (int j = 0; j < currentList.size(); j++) {
+                System.out.print("\t"+currentList.get(j)+"\t\t|");
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
