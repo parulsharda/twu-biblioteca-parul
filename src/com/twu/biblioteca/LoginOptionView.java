5c2581218c50f109ad470b1e75b3d2824892fccc
parulsharda <sharmanancy007@gmail.com>
2015-08-07 17:46:05 +0530

    Application accepts username and password from user nad redirects the user to appropriate menu screen based on role

--- src/com/twu/biblioteca/LoginOptionView.java
+++ src/com/twu/biblioteca/LoginOptionView.java
@@ -3,31 +3,50 @@ package com.twu.biblioteca;
 public class LoginOptionView implements ViewInterface {
     Scanner scanner;
     ViewParser viewParser;
+    ViewInterface mainLoginMenuView;
     ViewInterface userDisplayMenuView;
+    ViewInterface librarianDisplayMenuView;
     AuthenticateLogin authenticateLogin;
-    private String username;
-    private String password;
+    private Users currentUser;
 
 
-    public LoginOptionView(Scanner scanner, ViewParser viewParser, AuthenticateLogin authenticateLogin, ViewInterface userDisplayMenuView) {
+
+    public LoginOptionView(Scanner scanner, ViewParser viewParser, AuthenticateLogin authenticateLogin, ViewInterface userDisplayMenuView,ViewInterface mainLoginMenuView,ViewInterface librarianDisplayMenuView) {
         this.scanner = scanner;
         this.viewParser = viewParser;
         this.authenticateLogin = authenticateLogin;
+        this.mainLoginMenuView = mainLoginMenuView;
         this.userDisplayMenuView = userDisplayMenuView;
+        this.librarianDisplayMenuView = librarianDisplayMenuView;
+        this.currentUser = null;
     }
 
     @Override
     public void draw() {
 
         System.out.println("Enter Username:\n");
-        username = scanner.acceptInput();
+        String username = scanner.acceptInput();
         System.out.print("Enter Password:\n");
-        password = scanner.acceptInput();
+        String password = scanner.acceptInput();
+        currentUser = authenticateLogin.check(username,password);
     }
 
     @Override
     public ViewInterface proceed()
     {
-        return null;
+        if(currentUser == null) {
+            return mainLoginMenuView;
+        }
+        else {
+            if (!currentUser.isAdmin()) {
+                return userDisplayMenuView;
+            }
+            else
+            {
+                return librarianDisplayMenuView;
+            }
+        }
     }
+
+
 }
--- test/com/twu/biblioteca/LoginOptionViewTest.java
+++ test/com/twu/biblioteca/LoginOptionViewTest.java
@@ -28,17 +28,35 @@ public class LoginOptionViewTest {
     public void checkTheinputAcceptingMessage() {
         ViewInterface userDisplayMenuView = Mockito.mock(UserDisplayMenuView.class);
         Scanner scanner = Mockito.mock(Scanner.class);
+        ViewInterface mainLoginMenuView = Mockito.mock(MainLoginMenuView.class);
+        ViewInterface librarianDisplayMenuView = Mockito.mock(LibrarianDisplayMenuView.class);
         ArrayList<Users> users = new ArrayList<Users>();
         Users user1 = new Users("999-1919","nancy","Admin","nancysharma@gmail.com","9890976754");
         Users user2  = new Users("111-1919","parul", "User","parulsharma@gmail.com","989796959");
         users.add(user1);
         users.add(user2);
-        AuthenticateLogin authenticateLogin = new AuthenticateLogin(users);
+        AuthenticateLogin authenticateLogin = Mockito.mock(AuthenticateLogin.class);
         ViewParser viewParser = Mockito.mock(ViewParser.class);
 
-        LoginOptionView loginOptionView = new LoginOptionView(scanner,viewParser,authenticateLogin,userDisplayMenuView);
+        LoginOptionView loginOptionView = new LoginOptionView(scanner,viewParser,authenticateLogin,userDisplayMenuView,mainLoginMenuView,librarianDisplayMenuView);
 
         loginOptionView.draw();
-        assertEquals("Enter Username:\n\nEnter Password:\n",outContent.toString());
+        assertEquals("Enter Username:\n\nEnter Password:\n", outContent.toString());
+    }
+
+
+    @Test
+    public void shouldProceedToNextViewBasedOnRoleOfUser() {
+        Scanner scanner = Mockito.mock(Scanner.class);
+        ViewParser viewParser = Mockito.mock(ViewParser.class);
+        AuthenticateLogin authenticateLogin = Mockito.mock(AuthenticateLogin.class);
+        ViewInterface userDisplayMenuView = Mockito.mock(UserDisplayMenuView.class);
+        ViewInterface mainLoginMenuView = Mockito.mock(MainLoginMenuView.class);
+        ViewInterface librarianDisplayMenuView = Mockito.mock(LibrarianDisplayMenuView.class);
+
+        LoginOptionView loginOptionView = new LoginOptionView(scanner,viewParser, authenticateLogin, userDisplayMenuView,mainLoginMenuView,librarianDisplayMenuView);
+
+        assertEquals(mainLoginMenuView, loginOptionView.proceed());
     }
 }
+
